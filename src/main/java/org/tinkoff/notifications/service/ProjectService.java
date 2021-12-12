8c7c5ca32fac91314937c5e5fafe3f51bb96b0be
DeveloPOOr <danil.chikilev@mail.ru>
2021-12-12 06:24:05 +0500

    Project Service with logger was added

--- src/main/java/org/tinkoff/notifications/service/ProjectService.java
+++ src/main/java/org/tinkoff/notifications/service/ProjectService.java
@@ -1,13 +1,43 @@
 package org.tinkoff.notifications.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tinkoff.notifications.dao.ProjectDao;
+import org.tinkoff.notifications.dto.ProjectDto;
+import org.tinkoff.notifications.model.Employee;
+import org.tinkoff.notifications.model.Project;
 
 @Service
 public class ProjectService {
 
     private ProjectDao projectDao;
+    private Logger logger = LoggerFactory.getLogger(ProjectService.class);
 
-    
+    public ProjectService(ProjectDao projectDao) {
+        this.projectDao = projectDao;
+    }
+
+    public void save(ProjectDto projectDto) {
+        projectDao.save(projectDto);
+        logger.info("Project " + projectDto + " was saved");
+    }
+
+    public Project findById(long id) {
+        Project project = projectDao.findById(id);
+        logger.info("Project with id " + id + " was found");
+        return project;
+    }
+
+    public void update(Project project) {
+        projectDao.update(project);
+        logger.info("Project " + project + " was updated");
+    }
+
+    public void delete(Project project) {
+        projectDao.delete(project);
+        logger.info("Project " + project + " was deleted");
+    }
+
 }
