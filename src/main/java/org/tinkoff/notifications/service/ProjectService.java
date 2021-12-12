a76678c081f265a2ff494b9dbca166446dcc0348
DeveloPOOr <danil.chikilev@mail.ru>
2021-12-12 05:12:40 +0500

    proj service start

---
+++ src/main/java/org/tinkoff/notifications/service/ProjectService.java
@@ -0,0 +1,13 @@
+package org.tinkoff.notifications.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tinkoff.notifications.dao.ProjectDao;
+
+@Service
+public class ProjectService {
+
+    private ProjectDao projectDao;
+
+    
+}
