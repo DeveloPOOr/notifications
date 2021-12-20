098cd775c46f8a6a5bb89b66b16e37b79f28f243
DeveloPOOr <danil.chikilev@mail.ru>
2021-12-20 04:10:04 +0300

    edit tests

--- src/test/java/org/tinkoff/notifications/dao/NotificationDaoTest.java
+++ src/test/java/org/tinkoff/notifications/dao/NotificationDaoTest.java
@@ -3,6 +3,7 @@ package org.tinkoff.notifications.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.tinkoff.notifications.dto.NotificationDto;
 import org.tinkoff.notifications.model.Employee;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public
 class NotificationDaoTest {
 
--- src/test/java/org/tinkoff/notifications/dao/PresentDaoTest.java
+++ src/test/java/org/tinkoff/notifications/dao/PresentDaoTest.java
@@ -3,6 +3,7 @@ package org.tinkoff.notifications.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.tinkoff.notifications.dto.PresentDto;
 import org.tinkoff.notifications.model.Present;
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public
 class PresentDaoTest {
 
